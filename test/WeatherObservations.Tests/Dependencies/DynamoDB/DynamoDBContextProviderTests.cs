4ed044b892dc11585678337df7c7839cbe0636d6
Kyle Hurd <kyle.hurd@wsu.edu>
2023-04-13 02:09:41 -0700

    Adds wrapper for the DynamoDBProviderTest to pass

--- test/WeatherObservations.Tests/Dependencies/DynamoDB/DynamoDBContextProviderTests.cs
+++ test/WeatherObservations.Tests/Dependencies/DynamoDB/DynamoDBContextProviderTests.cs
@@ -1,5 +1,7 @@
+using Amazon;
 using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DataModel;
+using Amazon.Runtime;
 using Moq;
 using Ninject.Activation;
 using WeatherObservations.Dependencies.DynamoDB;
@@ -21,11 +23,16 @@ internal class TestableDynamoDBContextProvider : DynamoDBContextProvider
     }
 }
 
+public class AmazonDynamoDBClientWrapper : AmazonDynamoDBClient
+{
+    public virtual IClientConfig ConfigWrapper => base.Config;
+}
+
 public class DynamoDBContextProviderTests
 {
     private TestableDynamoDBContextProvider DynamoDBContextProvider { get; set; }
 
-    private Mock<AmazonDynamoDBClient> Client { get; set; }
+    private Mock<AmazonDynamoDBClientWrapper> Client { get; set; }
 
     private Mock<DynamoDBContextConfig> Config { get; set; }
 
@@ -39,9 +46,19 @@ public class DynamoDBContextProviderTests
     [Fact]
     public void TestCreateInstance()
     {
+        // Error: No RegionEndpoint or ServiceURL configured
+        this.Client.SetupGet(x => x.ConfigWrapper.RegionEndpoint)
+            .Returns(RegionEndpoint.USEast1)
+            .Verifiable();
+        
+        this.Client.SetupGet(x => x.ConfigWrapper.ServiceURL)
+            .Returns("http://localhost:8000")
+            .Verifiable();
+
         var context = this.DynamoDBContextProvider.CreateInstance(null!);
 
         Assert.NotNull(context);
         Assert.IsType<DynamoDBContext>(context);
+        Mock.Verify();
     }
 }
