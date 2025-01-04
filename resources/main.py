321ebc2c620c4d349c5d73f06680a75e2f532a20
Kyle Hurd <kmdude1000@gmail.com>
2025-01-04 14:01:14 -0800

    Adds more dropzones to interaction model and adjusts Alexa responses (#22)

--- resources/main.py
+++ resources/main.py
@@ -7,7 +7,7 @@ response = ssm.get_parameter(Name='/SkyBro/CheckWXClient/ApiKey', WithDecryption
 api_key = response['Parameter']['Value']
 
 url = 'https://api.checkwx.com/metar/KSHN/decoded'
-url = 'https://api.checkwx.com/metar/lat/47.24216/lon/-123.14186/decoded'
-url = 'https://api.checkwx.com/metar/KRDU/decoded'
+url = 'https://api.checkwx.com/metar/lat/37.5108611/lon/-120.0395278/decoded'
+# url = 'https://api.checkwx.com/metar/KSHN/decoded'
 response = requests.request('GET', url, headers= {'X-API-Key': api_key})
 print(json.dumps(json.loads(response.text), indent=4))
